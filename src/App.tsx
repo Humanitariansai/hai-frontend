a4d2fa18b18228638804dfa840db67d8ef1b2b5d
PS8924 <shah.pankti@northeastern.edu>
2024-10-15 15:29:59 -0700

    Basic core project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Humans + AI {'>'} AI</h1>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
