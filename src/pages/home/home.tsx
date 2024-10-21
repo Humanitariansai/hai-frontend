137785b10e9982d5cee06d0e601e9078b62aa1f6
PS8924 <shah.pankti@northeastern.edu>
2024-10-20 18:40:58 -0700

    added feature card skeleton component and feature conatiner component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroScreen from '../../components/hero';
 import '../../app/css/index.css';
 import { NAV_LINKS } from '../../constants/index';
+import FeatureContainer from '../../components/featureContainer';
 
 const Home = () => {
   const homeTitle = NAV_LINKS.find((element) => element.label === 'Home')?.title || 'Default Title';
   return (
    <>
     <HeroScreen title={homeTitle}/>
+    <FeatureContainer/>
    </>
   );
 };
